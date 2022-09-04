456bfe6b8389c86f1beeda8d498abf94267962d3
nubenunca <marylinrestrepp@gmail.com>
2022-09-04 16:33:20 -0500

    saludando al mundo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World, SOY MARYLIN
+          
         </p>
         <a
           className="App-link"
@@ -23,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+
